a5a8d4fb67cbb40eac2f327dcc4ad1ce5ac14c48
RollingSaqartvelo <rolling_saqartvelo@outlook.com>
2026-05-17 18:24:02 +0400

    Layout: full-page scroll instead of footer-only scroll

--- lava-platform/frontend/dist/assets/BufferResource-DD6qRMix.js
+++ lava-platform/frontend/dist/assets/BufferResource-DD6qRMix.js
@@ -1,4 +1,4 @@
-import{w as U,a as g,S as c,u as S,B as _,b as m,E as I,c as p}from"./index-BxbdF8v3.js";const x={name:"local-uniform-bit",vertex:{header:`
+import{w as U,a as g,S as c,u as S,B as _,b as m,E as I,c as p}from"./index-BdJ3H5Pf.js";const x={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
