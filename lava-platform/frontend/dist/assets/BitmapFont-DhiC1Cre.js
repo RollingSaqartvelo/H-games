ce214494156249e156c5082e98faf0e8c18a122f
RollingSaqartvelo <rolling_saqartvelo@outlook.com>
2026-06-07 02:02:25 +0400

    Outlaw: panel flex-fill footer (no aspect-ratio, 100% width/height)

--- lava-platform/frontend/dist/assets/BitmapFont-DhiC1Cre.js
+++ lava-platform/frontend/dist/assets/BitmapFont-DhiC1Cre.js
@@ -1,4 +1,4 @@
-import{D as nt,T as J,C as U,M as ft,j as at,F as et,w as ht,E as Ft,c as Pt,o as X,t as pt,p as gt,q as N,v as $,r as St,x as rt,I as Mt,y as R,g as It}from"./index-BPLatkia.js";import{C as xt}from"./CanvasPool-Qj6A2XHN.js";import"./zustand-DgPfcmaj.js";import"./react-DWZXVlP3.js";/**
+import{D as nt,T as J,C as U,M as ft,j as at,F as et,w as ht,E as Ft,c as Pt,o as X,t as pt,p as gt,q as N,v as $,r as St,x as rt,I as Mt,y as R,g as It}from"./index-DRadxonE.js";import{C as xt}from"./CanvasPool-0JEYQufM.js";import"./zustand-DgPfcmaj.js";import"./react-DWZXVlP3.js";/**
  * tiny-lru
  *
  * @copyright 2026 Jason Mulligan <jason.mulligan@avoidwork.com>
