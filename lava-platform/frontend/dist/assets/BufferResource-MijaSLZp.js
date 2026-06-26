5fa1286c8cf246f78a2874516a9859cc8f5ecfd1
RollingSaqartvelo <rolling_saqartvelo@outlook.com>
2026-06-26 10:03:57 +0400

    Badges: 3D PNG state badges in Outlaw + Bubble + Granny; granny crash text no 'Doctor'

--- lava-platform/frontend/dist/assets/BufferResource-MijaSLZp.js
+++ lava-platform/frontend/dist/assets/BufferResource-MijaSLZp.js
@@ -1,4 +1,4 @@
-import{w as U,a as g,S as c,u as S,B as _,b as m,E as I,c as p}from"./index-D7f1wFvu.js";const x={name:"local-uniform-bit",vertex:{header:`
+import{w as U,a as g,S as c,u as S,B as _,b as m,E as I,c as p}from"./index-CUK9tPoT.js";const x={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
