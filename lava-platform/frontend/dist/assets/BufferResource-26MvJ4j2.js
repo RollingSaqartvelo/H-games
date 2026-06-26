aa48e608b6a131e6b9c3cf7dffb8b486cc32be8e
RollingSaqartvelo <rolling_saqartvelo@outlook.com>
2026-06-26 22:02:06 +0400

    Outlaw: amount input +10% wider again

--- lava-platform/frontend/dist/assets/BufferResource-26MvJ4j2.js
+++ lava-platform/frontend/dist/assets/BufferResource-26MvJ4j2.js
@@ -1,4 +1,4 @@
-import{w as U,a as g,S as c,u as S,B as _,b as m,E as I,c as p}from"./index-C_2Bi5XL.js";const x={name:"local-uniform-bit",vertex:{header:`
+import{w as U,a as g,S as c,u as S,B as _,b as m,E as I,c as p}from"./index-Dw4YgB4B.js";const x={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
