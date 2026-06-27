eb560411ebb1a0492b20be2ff1fb5f37fe23c2d3
RollingSaqartvelo <rolling_saqartvelo@outlook.com>
2026-06-27 21:27:48 +0400

    Outlaw: hide broadcast ticker strip; AUTO BET overflow fix +40px right; $1 -10px; mute left of state badge

--- lava-platform/frontend/dist/assets/BitmapFont-DYqt8JXR.js
+++ lava-platform/frontend/dist/assets/BitmapFont-DYqt8JXR.js
@@ -1,4 +1,4 @@
-import{D as nt,T as J,C as U,M as ft,j as at,F as et,w as ht,E as Ft,c as Pt,o as X,t as pt,p as gt,q as N,v as $,r as St,x as rt,I as Mt,y as R,g as It}from"./index-C_ofcSYv.js";import{C as xt}from"./CanvasPool-n5kLMPV2.js";import"./zustand-DgPfcmaj.js";import"./react-DWZXVlP3.js";/**
+import{D as nt,T as J,C as U,M as ft,j as at,F as et,w as ht,E as Ft,c as Pt,o as X,t as pt,p as gt,q as N,v as $,r as St,x as rt,I as Mt,y as R,g as It}from"./index-B_Eni4DL.js";import{C as xt}from"./CanvasPool-Zmh7b6_k.js";import"./zustand-DgPfcmaj.js";import"./react-DWZXVlP3.js";/**
  * tiny-lru
  *
  * @copyright 2026 Jason Mulligan <jason.mulligan@avoidwork.com>
--- lava-platform/frontend/dist/assets/BufferResource-BfAql1f1.js
+++ lava-platform/frontend/dist/assets/BufferResource-BfAql1f1.js
@@ -1,4 +1,4 @@
-import{w as U,a as g,S as c,u as S,B as _,b as m,E as I,c as p}from"./index-C_ofcSYv.js";const x={name:"local-uniform-bit",vertex:{header:`
+import{w as U,a as g,S as c,u as S,B as _,b as m,E as I,c as p}from"./index-B_Eni4DL.js";const x={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
