8f6defe0f8e0095bdf5d7111ff540564b57b3880
RollingSaqartvelo <rolling_saqartvelo@outlook.com>
2026-06-27 10:53:54 +0400

    Outlaw: $1 and $5 chips 3px left

--- lava-platform/frontend/dist/assets/BufferResource-Dpaxwhud.js
+++ lava-platform/frontend/dist/assets/BufferResource-Dpaxwhud.js
@@ -1,4 +1,4 @@
-import{w as U,a as g,S as c,u as S,B as _,b as m,E as I,c as p}from"./index-DanArHgh.js";const x={name:"local-uniform-bit",vertex:{header:`
+import{w as U,a as g,S as c,u as S,B as _,b as m,E as I,c as p}from"./index-BhnhuMda.js";const x={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
