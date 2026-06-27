0b46f48edfbcd8a53a6a1694937f6fc0febc284b
RollingSaqartvelo <rolling_saqartvelo@outlook.com>
2026-06-27 10:39:44 +0400

    Outlaw: chips/AUTO BET +40px right -20px up; AUTO BET label two lines

--- lava-platform/frontend/dist/assets/BitmapFont-ne7w6u-T.js
+++ lava-platform/frontend/dist/assets/BitmapFont-ne7w6u-T.js
@@ -1,4 +1,4 @@
-import{D as nt,T as J,C as U,M as ft,j as at,F as et,w as ht,E as Ft,c as Pt,o as X,t as pt,p as gt,q as N,v as $,r as St,x as rt,I as Mt,y as R,g as It}from"./index-Danca_-H.js";import{C as xt}from"./CanvasPool-QXXsSBdb.js";import"./zustand-DgPfcmaj.js";import"./react-DWZXVlP3.js";/**
+import{D as nt,T as J,C as U,M as ft,j as at,F as et,w as ht,E as Ft,c as Pt,o as X,t as pt,p as gt,q as N,v as $,r as St,x as rt,I as Mt,y as R,g as It}from"./index-zhZ99hjG.js";import{C as xt}from"./CanvasPool-QR5LoWDZ.js";import"./zustand-DgPfcmaj.js";import"./react-DWZXVlP3.js";/**
  * tiny-lru
  *
  * @copyright 2026 Jason Mulligan <jason.mulligan@avoidwork.com>
